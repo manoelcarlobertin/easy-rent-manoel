39dfcfd20f9984af29dadc510ef978b1b47627fb
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-13 10:40:07 -0300

    validates no model customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :mobile_phone, presence: true
+  validates :email, presence: true
+  # def self.ransackable_attributes(auth_object = nil)
+  #   ["name"]
+  # end
 end
