3c1333edce2acb9cc1ffd9445e722e595ffba28b
Manoel Carlo Bertin <manoelcarlobertin@outlook.com>
2024-09-13 22:16:22 -0300

    bundle install e db migrate done.

--- db/migrate/20240914010455_create_equipment.rb
+++ db/migrate/20240914010455_create_equipment.rb
@@ -1,8 +1,8 @@
 class CreateEquipment < ActiveRecord::Migration[7.2]
   def change
     create_table :equipment do |t|
-      t.string :name
-      t.string :serial_number, unique: true
+      t.string :name, null: false
+      t.string :serial_number, unique: true, null: false
 
       t.timestamps
     end
